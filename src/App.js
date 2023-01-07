e4e85423f231797e49c34301e54da7eb442a5ca6
Rishabh-dot-tomar <70138890+Rishabh-dot-tomar@users.noreply.github.com>
2023-01-07 21:10:08 +0530

    right pannel added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
-// import './App.css';
+import './Appp.css';
 import LeftMenu from './components/LeftMenu';
 import MainPage from './components/MainComponnent/MainPage';
 
 function App() {
   return (
+    <div className='App'>
+      <div className='leftPannel'>
+        <LeftMenu/>
+      </div>
+      <div className='rightPannel'>
+        <MainPage/>
+      </div>
 
-    <>
-    <LeftMenu/>
-    <MainPage/>
-    </>
+    </div>
   );
 }
 
--- src/components/MainComponnent/MainPage.js
+++ src/components/MainComponnent/MainPage.js
@@ -5,6 +5,38 @@ function MainPage() {
     <div>
         <div className='userPannel'>
            <span className='userName'>Hi, Abhishek</span>
+           <image src='' alt ="user image"/>
+
+        </div>
+        <div className='detailsPannel'>
+            <img className='imageWrapperr'src='https://cdn-icons-png.flaticon.com/512/3997/3997608.png' img='pdf'/>
+            <span>Lucas films </span>
+
+            <div className='detailswrapper'>
+                <div className='detailsTitle'>
+                    <span>Company Code</span>
+                    <span>Domains</span>
+                </div>
+                <div className='emailsWrapper'>
+                    <div>
+                        <span>LUCF</span>
+                    </div>
+                    <div>
+                        <span>@lucasFilms.com</span><br/>
+                        <span>@lucasstarwars.com</span>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <span className='detailsTitle'>Company Logo</span>
+                <span>unavailable</span>
+                <hr/>
+            </div>
+            <div className='detailsTitle'>
+                <span>Entities</span>
+                <span>Invoice Codes</span>
+                <span>Users</span>
+            </div>
 
         </div>
 
